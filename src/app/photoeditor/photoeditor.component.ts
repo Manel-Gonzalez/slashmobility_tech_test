b9f833d7796ef52a79dd0f6a98863b2cc5a1ca37
Manel <manel.gonzalez.rico@gmail.com>
2021-09-29 01:10:16 +0200

    ready to merge

---
+++ src/app/photoeditor/photoeditor.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photoeditor',
+  templateUrl: './photoeditor.component.html',
+  styleUrls: ['./photoeditor.component.css']
+})
+export class PhotoeditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  url = '';
+
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]);
+
+      reader.onload = (event) => { 
+        this.url = event.target.result;
+      }
+    }
+  }
+  public delete(){
+    this.url = null;
+    
+  }
+}
